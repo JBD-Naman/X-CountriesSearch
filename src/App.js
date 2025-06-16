6587e25cecff2634312f9ba01b369df017510825
Your Name <158471535+JBD-Naman@users.noreply.github.com>
2025-06-16 10:21:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     // Fetch country data from API
     const fetchCountries = async () => {
       try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
+        const response = await fetch("https://xcountries-backend.azurewebsites.net/all");
         if (!response.ok) throw new Error("Network response was not ok.");
         const data = await response.json();
         setCountries(data);
